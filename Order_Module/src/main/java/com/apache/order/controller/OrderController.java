d06e2a31755edb3d083886c6976e025be2b9bf70
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-15 15:03:24 +0530

    Adding OrderController in Order_module

--- Order_Module/src/main/java/com/apache/order/controller/OrderController.java
+++ Order_Module/src/main/java/com/apache/order/controller/OrderController.java
@@ -1,12 +1,18 @@
 package com.apache.order.controller;
 
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apache.domains.dto.Order;
+import com.apache.domains.dto.OrderEvent;
 import com.apache.order.services.OrderProducerService;
 
 @RestController
-@RequestMapping("/api/order")
+@RequestMapping("/api/v2")
 public class OrderController {
 
 	private OrderProducerService orderProducerService;
@@ -16,5 +22,18 @@ public class OrderController {
 		this.orderProducerService = orderProducerService;
 	}
 	
-	
+	@PostMapping("/order")
+	public String placeOrder(@RequestBody Order order) {
+		
+		order.setOrderId(UUID.randomUUID().toString());
+		
+		OrderEvent orderEvent = new OrderEvent();
+		orderEvent.setStatus("Order Pending");
+		orderEvent.setMessage("Please confirm your order.!");
+		orderEvent.setOrder(order);
+		
+		orderProducerService.sendMessage(orderEvent);
+		
+		return "Order Placed Successfully.!";
+	}
 }
