915eed7e99632d980d3687ea9550de4a6876d4c4
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-15 13:52:47 +0530

    Adding OrderProducerservice in Order_module

--- Order_Module/src/main/java/com/apache/order/services/OrderProducerService.java
+++ Order_Module/src/main/java/com/apache/order/services/OrderProducerService.java
@@ -1,8 +1,42 @@
 package com.apache.order.services;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+
+import com.apache.domains.dto.OrderEvent;
 
 @Service
 public class OrderProducerService {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrderProducerService.class);
 
+	private NewTopic topic;
+	
+	private KafkaTemplate<String, OrderEvent> kafkaTemplate;
+
+	// Using constructor based dependency injection
+	public OrderProducerService(NewTopic topic, KafkaTemplate<String, OrderEvent> kafkaTemplate) {
+		super();
+		this.topic = topic;
+		this.kafkaTemplate = kafkaTemplate;
+	}
+
+	public void sendMessage(OrderEvent orderEvent) {
+		
+		LOGGER.info(String.format("Order event : %s", orderEvent.toString()));
+		
+		// create message
+		Message<OrderEvent> message = MessageBuilder
+										.withPayload(orderEvent)
+										.setHeader(KafkaHeaders.TOPIC, topic.name())
+										.build();
+		
+		kafkaTemplate.send(message);
+	}
 }
