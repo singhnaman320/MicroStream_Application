3e2ea7650af924c1b218b2ff5efd27905698d8e3
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-15 14:05:38 +0530

    Adding OrderController in Order_module

--- Order_Module/src/main/java/com/apache/order/controller/OrderController.java
+++ Order_Module/src/main/java/com/apache/order/controller/OrderController.java
@@ -3,8 +3,18 @@ package com.apache.order.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apache.order.services.OrderProducerService;
+
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
 
+	private OrderProducerService orderProducerService;
+
+	public OrderController(OrderProducerService orderProducerService) {
+		super();
+		this.orderProducerService = orderProducerService;
+	}
+	
+	
 }
