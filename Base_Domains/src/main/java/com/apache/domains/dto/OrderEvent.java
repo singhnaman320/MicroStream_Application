77ad0634a22377d0341e56e3cb97e72932231530
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-15 11:21:02 +0530

    Adding OrderEvent entity in base domain

--- Base_Domains/src/main/java/com/apache/domains/dto/OrderEvent.java
+++ Base_Domains/src/main/java/com/apache/domains/dto/OrderEvent.java
@@ -1,6 +1,18 @@
 package com.apache.domains.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 // This is the entity we will use to transfer the data between producer and consumer using Apache Kafka
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderEvent {
 
+	private String message;
+	private String status;
+	private Order order;
+	
 }
