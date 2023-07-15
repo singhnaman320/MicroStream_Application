21211c5e0b78c60bc6cffade82997284e73b68bc
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-15 18:55:03 +0530

    Adding Placed orders entity in stockmodule

---
+++ Stock_Module/src/main/java/com/apache/stock/databaseModel/OrderStore.java
@@ -0,0 +1,20 @@
+package com.apache.stock.databaseModel;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Placed_Orders")
+public class OrderStore {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long orderId;
+	
+	@Lob
+	private String stockEvent;
+}
