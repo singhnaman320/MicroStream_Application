2b1929ea8db84087a8f8c9d01271fccb9d7deac2
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-15 12:49:09 +0530

    Adding KafkaTopicConfig in Order_module

--- Order_Module/src/main/java/com/apache/order/config/KafkaTopicConfig.java
+++ Order_Module/src/main/java/com/apache/order/config/KafkaTopicConfig.java
@@ -1,5 +1,23 @@
 package com.apache.order.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaTopicConfig {
 
+	@Value("${spring.kafka.topic.name}")
+	private String topicName;
+	
+	// Spring Bean for Kafka Topic
+	@Bean
+	public NewTopic topic() {
+		
+		return TopicBuilder
+				.name(topicName)
+				.build();
+	}
 }
