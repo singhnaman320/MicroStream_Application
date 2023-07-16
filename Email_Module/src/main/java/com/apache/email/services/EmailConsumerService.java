78035bda668b696d967848e5eab56dd20e364de2
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-16 13:21:06 +0530

    Modifying EmailSenderService in Email Module

--- Email_Module/src/main/java/com/apache/email/services/EmailConsumerService.java
+++ Email_Module/src/main/java/com/apache/email/services/EmailConsumerService.java
@@ -8,6 +8,7 @@ import org.springframework.context.event.EventListener;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.apache.domains.dto.Order;
 import com.apache.domains.dto.OrderEvent;
 
 @Service
@@ -21,7 +22,11 @@ public class EmailConsumerService {
 	@EventListener(ApplicationReadyEvent.class)
 	public void sendEmail() {
 		
-		senderService.sendEmail("singhnaman620@gmail.com", "Order Confirmation.!", "Your Order has been confirmed");
+		senderService.sendEmail("singhnaman620@gmail.com", 
+				"Order Confirmation.!", 
+				"Thank you for your order. We’ll send a confirmation when your order ships. "
+				+ "Your estimated delivery date is indicated below. If you would like to view the status of your order "
+				+ "or make any changes to it, please visit Your Orders");
 	}
 	
 	@KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
