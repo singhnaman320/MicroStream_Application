e9216f71ba840ac11d29fd58e5ee1e791057bfbf
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-16 12:13:55 +0530

    Adding EmailSenderService in Email Module

--- Email_Module/src/main/java/com/apache/email/services/EmailSenderService.java
+++ Email_Module/src/main/java/com/apache/email/services/EmailSenderService.java
@@ -1,8 +1,12 @@
 package com.apache.email.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailSenderService {
 
+	@Autowired
+	private JavaMailSender mailSender;
 }
